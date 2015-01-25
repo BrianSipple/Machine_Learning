c22707a65177eea9c467e2ecc24d5f1552488abf
Brian Sipple <Bsipple57@gmail.com>
2015-01-25 11:11:35 -0600

    svm predictions: counting predictions with rbf classifier

--- text_learning_exercise/svm_author_id.py
+++ text_learning_exercise/svm_author_id.py
@@ -24,15 +24,37 @@ import numpy as np
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
+# (Optionally) Slim down the training set to speed things up
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
 X = np.array(features_train)
 Y = np.array(labels_train)
 
-clf = svm.SVC(kernel="linear")
+clf = svm.SVC(kernel="rbf", C=10000)
 clf.fit(X, Y)
 
+pred_time_0 = time()
 predictions = clf.predict(features_test)
+pred_time_1 = time()
 
+print "Computed predictions in time {}".format(pred_time_1 - pred_time_0)
 
+acc_time_0 = time()
 accuracy = accuracy_score(predictions, labels_test)
+acc_time_1 = time()
+
+print "Computed accuracy in time {}".format(acc_time_1 - acc_time_0)
 
 print "Accuracy: {}".format(accuracy)
+
+sara_emails = 0
+chris_emails = 0
+for i in range(len(predictions)):
+    if predictions[i] == 0:
+        sara_emails += 1
+    elif predictions[i] == 1:
+        chris_emails += 1
+
+print "Number of emails predicted for Chris: {}".format(chris_emails)
+print "Number of emails predicted for Sara: {}".format(sara_emails)
