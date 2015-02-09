f0848183dba64000ff26a32ec45e97531d2bc758
Brian Sipple <Bsipple57@gmail.com>
2015-02-08 22:17:40 -0600

    function for feature scaling

--- enron_dataset_project/kmeans.py
+++ enron_dataset_project/kmeans.py
@@ -23,6 +23,27 @@ def get_min_max_values_for_feature(data_dict, feature):
     return (feature_vals[0], feature_vals[-1])
 
 
+def scale_features(values):
+
+    res = []
+
+    min_val = np.min(values)
+    max_val = np.max(values)
+    val_range = max_val - min_val
+
+    if min_val == max_val:  # Prevent division by zero if min and max are equal
+        return values
+
+    for i in range(len(values)):
+        scaled_val = float(values[i] - min_val)) / float(val_range)
+        res.append(scaled_val)
+
+    return res
+
+
+
+
+
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
     """ some plotting code designed to help you visualize your clusters """
@@ -71,7 +92,7 @@ for f1, f2, _ in finance_features:
 
 plt.xlabel(finance_features[0])
 plt.ylabel(finance_features[1])
-#########plt.show()
+plt.show()
 
 
 
@@ -81,19 +102,20 @@ data2 = featureFormat(data_dict, features_list )
 poi, finance_features = labelFeatureSplit( data2 )
 clf = KMeans(n_clusters=3)
 pred = clf.fit_predict( finance_features )
-###########Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
+Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
 
 
-### cluster here; create predictions of the cluster labels
-### for the data and store them to a list called pred
+## cluster here; create predictions of the cluster labels
+## for the data and store them to a list called pred
 
-# try:
-#     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
-# except NameError:
-#     print "no predictions object named pred found, no clusters to plot"
+try:
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+except NameError:
+    print "no predictions object named pred found, no clusters to plot"
 
 
 if __name__ == '__main__':
+
 
     print get_min_max_values_for_feature(data_dict, 'exercised_stock_options')
     print get_min_max_values_for_feature(data_dict, 'salary')
